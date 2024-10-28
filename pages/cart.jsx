a3ded8936031f3e7b323abf84e9b4345612b25d7
Rony Boscan <58662742+ronyx-b@users.noreply.github.com>
2024-10-28 19:16:59 -0400

    updated account and cart views and related services

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,6 +1,8 @@
 import CartCardItem from "@/components/Cart/CartCardItem";
 import CartProductItem from "@/components/Cart/CartProductItem";
-import { SERVER_URL } from "@/config";
+import useCardsFromCollection from "@/services/cache/useCardsFromCollection";
+import useProductsFromCollection from "@/services/cache/useProductsFromCollection";
+import useUserProfile from "@/services/cache/useUserProfile";
 import { selectCart } from "@/services/store/cartSlice";
 import { selectDecodedToken, selectToken } from "@/services/store/tokenSlice";
 import { useRouter } from "next/router";
@@ -13,13 +15,15 @@ export default function Cart({ ...props }) {
   const cart = useSelector(selectCart);
   const token = useSelector(selectToken);
   const decodedToken = useSelector(selectDecodedToken);
-  const [cards, setCards] = useState([]);
-  const [sealed, setSealed] = useState([]);
+  const identifiers = cart.filter((item) => (item.type === "single")).map((item) => ({id: item.id}));
+  const cards = useCardsFromCollection({ identifiers });
+  const productIdList = cart.filter((item) => (item.type === "sealed")).map((item) => item.id);
+  const sealed = useProductsFromCollection({ productIdList });
   const [cartTotal, setCartTotal] = useState();
   const [orderProducts, setOrderProducts] = useState();
   const [emptyCartModal, setEmptyCartModal] = useState(false);
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(true);
+  const userProfile = useUserProfile(token);
 
   const handleClose = () => setEmptyCartModal(false);
   const handleShow = () => setEmptyCartModal(true);
@@ -29,35 +33,13 @@ export default function Cart({ ...props }) {
     handleClose();
   };
 
-  const getAccountAddress = async () => {
-    let requestString = `${SERVER_URL}/api/user`;
-    try {
-      let response = await fetch(requestString, { 
-        method: 'GET',
-        headers: { 
-          'Content-Type': 'application/json',
-          'Authorization': `JWT ${token}` 
-        } 
-      });
-      if (response.status >= 400) {
-        router.push('/login');
-      } else {
-        let json = await response.json();
-        return (json.user.address[json.user.defaultAddress]);
-      }
-    } catch (err) {
-      console.log(err);
-      return {};
-    }
-  }
-
   const checkout = async () => {
     if(!token) {
       console.log("You must be logged in");
       return;
     }
 
-    let address = await getAccountAddress()
+    let address = userProfile.data?.address?.[userProfile.data?.defaultAddress];
 
     let order = {
       user_id : decodedToken._id,
@@ -68,67 +50,25 @@ export default function Cart({ ...props }) {
 
     console.log(order);
 
-    let requestString = `${SERVER_URL}/api/checkout`;
-    let requestOptions = { 
-      method: 'POST', 
-      body: JSON.stringify(order), 
-      headers: { 
-        'Content-Type': 'application/json',
-        'Authorization': `JWT ${token}` 
-      }
-    };
-    let response = await fetch(requestString, requestOptions);
-    let json = await response.json();
-    console.log(json.message);
-    if (json.success) {
-      dispatch(emptyCart());
-    }
+    // let requestString = `${SERVER_URL}/api/checkout`;
+    // let requestOptions = { 
+    //   method: 'POST', 
+    //   body: JSON.stringify(order), 
+    //   headers: { 
+    //     'Content-Type': 'application/json',
+    //     'Authorization': `JWT ${token}` 
+    //   }
+    // };
+    // let response = await fetch(requestString, requestOptions);
+    // let json = await response.json();
+    // console.log(json.message);
+    // if (json.success) {
+    //   dispatch(emptyCart());
+    // }
   };
 
   useEffect(() => {
-    const getCardsData = async () => {
-      let requestString = `https://api.scryfall.com/cards/collection`;
-      let identifiers = cart.filter((item) => (item.type === "single")).map((item) => ({id: item.id}));
-      if (identifiers.length === 0) {
-        setCards([]);
-        return [];
-      }
-      let cardList = { identifiers };
-      let requestOptions = { 
-        method: 'POST', 
-        body: JSON.stringify(cardList), 
-        headers: { 'Content-Type': 'application/json'}
-      };
-      let response = await fetch(requestString, requestOptions);
-      let data = await response.json();
-      setCards(data.data);
-      return data.data;
-    };
-
-    const getSealedData = async () => {
-      let requestString = `${SERVER_URL}/api/products/collection`;
-      let products = cart.filter((item) => (item.type === "sealed")).map((item) => item.id);
-      if (products.length === 0) {
-        setSealed([]);
-        return ([]);
-      }
-      let requestOptions = { 
-        method: 'POST', 
-        body: JSON.stringify({ productIdList: products }), 
-        headers: { 'Content-Type': 'application/json'}
-      };
-      let response = await fetch(requestString, requestOptions);
-      let data = await response.json();
-      setSealed(data.products);
-      return (data.products);
-    };
-
-    const getAccessoriesData = async () => {
-      return ([]);
-    };
-
-    const getOrderProducts = (data) => {
-      let {cards, sealed, accessories} = data;
+    const getOrderProducts = () => {
       let products = cart.map(item => {
         let product = {
           prodType: item.type,
@@ -140,12 +80,12 @@ export default function Cart({ ...props }) {
         }
         switch(item.type) {
           case "single":
-            product.price = cards ? parseFloat(cards.find((card) => (card.id === item.id))?.prices.usd) : 0;
-            product.cardSet = cards ? cards.find((card) => (card.id === item.id))?.set_name : "";
+            product.price = parseFloat(cards.data?.find((card) => (card.id === item.id))?.prices.usd);
+            product.cardSet = cards.data?.find((card) => (card.id === item.id))?.set_name;
             break;
           case "sealed":
-            product.price = sealed ? parseFloat(sealed.find((prod) => (prod._id === item.id))?.price) : 0;
-            product.cardSet= sealed ? sealed.find((prod) => (prod._id === item.id))?.cardSet : "";
+            product.price = parseFloat(sealed.data?.find((prod) => (prod._id === item.id))?.price);
+            product.cardSet = sealed.data?.find((prod) => (prod._id === item.id))?.cardSet;
             break;
           case "accessory":
             break;
@@ -156,37 +96,22 @@ export default function Cart({ ...props }) {
         return product;
       });
       setOrderProducts(products);
-      return products;
-    };
-
-    const calculateCartTotal = (products) => { 
       let total = products.reduce((total, item) => { 
         return total += (item.price * item.qty); 
       }, 0)
       setCartTotal(total.toFixed(2));
     };
 
-    const loadCartData = async () => {
-      let data = {};
-      data.cards = await getCardsData();
-      data.sealed = await getSealedData();
-      data.accessories = await getAccessoriesData();
-      calculateCartTotal(getOrderProducts(data));
+    if (cards.data && sealed.data) {
+      getOrderProducts();
     }
 
-    try {
-      loadCartData().then(() => {
-        setIsLoading(false);
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  }, [cart]);
+  }, [cart, cards.data, sealed.data]);
 
   return (
     <div className="Cart" {...props}>
       <Container className="Cart">
-        {isLoading ? <>
+        {cards.isLoading || sealed.isLoading || userProfile.isLoading ? <>
           <Spinner animation="border" role="status" style={{ display: "block", margin: "5rem auto" }}>
             <span className="visually-hidden">Loading...</span>
           </Spinner>
@@ -199,10 +124,10 @@ export default function Cart({ ...props }) {
             {(cart && cart.length > 0)?
               <>
               {cart.filter((item) => (item.type === "sealed")).map((item) => 
-                <CartProductItem key={item.id} item={item} product={sealed?.find((product) => (product._id === item.id))} />
+                <CartProductItem key={item.id} item={item} product={sealed.data?.find((product) => (product._id === item.id))} />
               )}
               {cart.filter((item) => (item.type === "single")).map((item) => 
-                <CartCardItem key={item.id} item={item} card={cards?.find((card) => (card.id === item.id))} />
+                <CartCardItem key={item.id} item={item} card={cards.data?.find((card) => (card.id === item.id))} />
               )}
               <Row className="m-1 p-2 border-bottom">
                 <Col><h3>Cart Total</h3></Col>
